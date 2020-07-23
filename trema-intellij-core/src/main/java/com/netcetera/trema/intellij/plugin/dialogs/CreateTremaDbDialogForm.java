dfca3804e7cbdcd94b87ea3b16c0af7a3a249ee9
Lucas Jacques <Lucas.Jacques@netcetera.com>
2020-07-23 11:13:44 +0200

    Remove private $$$getFont$$$ method - Newer IntelliJ plugin versions add this method in via ASM, so class loading then fails because of a duplicate method

--- trema-intellij-core/src/main/java/com/netcetera/trema/intellij/plugin/dialogs/CreateTremaDbDialogForm.java
+++ trema-intellij-core/src/main/java/com/netcetera/trema/intellij/plugin/dialogs/CreateTremaDbDialogForm.java
@@ -143,25 +143,4 @@ public class CreateTremaDbDialogForm extends AbstractTremaForm<CreateNewTremaDbM
     model.setSchemaPath(tremaSchemaLocation.getText());
     return model;
   }
-
-  /**
-   * @noinspection ALL
-   */
-  private Font $$$getFont$$$(String fontName, int style, int size, Font currentFont) {
-    if (currentFont == null)
-      return null;
-    String resultName;
-    if (fontName == null) {
-      resultName = currentFont.getName();
-    } else {
-      Font testFont = new Font(fontName, Font.PLAIN, 10);
-      if (testFont.canDisplay('a') && testFont.canDisplay('1')) {
-        resultName = fontName;
-      } else {
-        resultName = currentFont.getName();
-      }
-    }
-    return new Font(resultName, style >= 0 ? style : currentFont.getStyle(), size >= 0 ? size : currentFont.getSize());
-  }
-
 }
--- trema-intellij-core/src/main/java/com/netcetera/trema/intellij/plugin/dialogs/ImportTremaForm.java
+++ trema-intellij-core/src/main/java/com/netcetera/trema/intellij/plugin/dialogs/ImportTremaForm.java
@@ -353,27 +353,7 @@ public class ImportTremaForm extends AbstractTremaForm<TremaImportModel> {
         }
       };
 
-  /**
-   * @noinspection ALL
-   */
-  private Font $$$getFont$$$(String fontName, int style, int size, Font currentFont) {
-    if (currentFont == null)
-      return null;
-    String resultName;
-    if (fontName == null) {
-      resultName = currentFont.getName();
-    } else {
-      Font testFont = new Font(fontName, Font.PLAIN, 10);
-      if (testFont.canDisplay('a') && testFont.canDisplay('1')) {
-        resultName = fontName;
-      } else {
-        resultName = currentFont.getName();
-      }
-    }
-    return new Font(resultName, style >= 0 ? style : currentFont.getStyle(), size >= 0 ? size : currentFont.getSize());
-  }
-
-  private class TremaDataModel extends DefaultTableModel {
+  private static class TremaDataModel extends DefaultTableModel {
 
     @Override
     public Class<?> getColumnClass(int i) {
